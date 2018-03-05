44b268644796d7a333ad2936c098ba0057f168b8
Ozgur <ozgr@live.com>
2018-03-05 16:00:06 +0300

    If there is no person object use fake object.

--- src/frontend/js/components/Messages.jsx
+++ src/frontend/js/components/Messages.jsx
@@ -10,49 +10,45 @@ import { connect } from 'react-redux'
 
 class Messages extends React.Component {
   render () {
-    const person = this.props.persons[this.props.selectedPersonId]
+    const person = this.props.persons[this.props.selectedPersonId] ||
+      { name: '', statusMessage: '', messages: [] }
 
-    if (typeof person === 'object') {
-      const person = this.props.persons[this.props.selectedPersonId]
-      const messages = person.messages.map((message, i) => {
-        return (
-          <div key={i} className={`message ${message.direction}`}>
-            {message.text}
-          </div>
-        )
-      })
-
+    const messages = person.messages.map((message, i) => {
       return (
-        <div id='messagesContainer'>
-          <div id='selectedPersonProfile'>
-            <div className='person'>
-              <div className='personLeft'>
-                <img
-                  className='personPicture'
-                  src={`https://randomuser.me/api/portraits/men/${this.props.selectedPersonId}.jpg`}
-                />
+        <div key={i} className={`message ${message.direction}`}>
+          {message.text}
+        </div>
+      )
+    })
+
+    return (
+      <div id='messagesContainer'>
+        <div id='selectedPersonProfile'>
+          <div className='person'>
+            <div className='personLeft'>
+              <img
+                className='personPicture'
+                src={`https://randomuser.me/api/portraits/men/${this.props.selectedPersonId}.jpg`}
+              />
+            </div>
+
+            <div className='personRight'>
+              <div className='personName'>
+                {person.name}
               </div>
 
-              <div className='personRight'>
-                <div className='personName'>
-                  {person.name}
-                </div>
-
-                <div className='personLastMessage'>
-                  {person.statusMessage}
-                </div>
+              <div className='personLastMessage'>
+                {person.statusMessage}
               </div>
             </div>
           </div>
-
-          <div id='messages'>
-            {messages}
-          </div>
         </div>
-      )
-    } else {
-      return <br />
-    }
+
+        <div id='messages'>
+          {messages}
+        </div>
+      </div>
+    )
   }
 }
 
